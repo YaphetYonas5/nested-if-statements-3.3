7a49f3d359cbd2f959283f0e2c73f9844ec72b86
YaphetYonas1 <85519133+YaphetYonas5@users.noreply.github.com>
2021-12-23 02:22:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;  //Needed for Scanner object
+
+class Main {
+  public static void main(String[] args) {
+   int year;    // holds a year
+
+      // Create a Scanner object for keyboard input.
+      Scanner console = new Scanner(System.in);
+
+      // Get the year.
+      System.out.print("Enter a year : ");
+      year = console.nextInt();
+
+      // Determine whether the year is leap year.
+      if (year % 4 == 0)
+      {
+         if (year % 100 == 0)
+         {
+            if (year % 400 == 0)
+            {
+               System.out.println("A leap year");
+            }
+            else
+            {
+               System.out.println("Not a leap year");
+            }
+         }
+         else
+         {
+            System.out.println("A leap year");
+         }
+      }
+      else
+      {
+         System.out.println("Not a leap year");
+      }
+   }
+}
